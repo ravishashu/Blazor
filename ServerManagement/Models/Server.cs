c7fa321dcbb87a82cf0fdd56cdbad66887350293
ravishashu <56905495+ravishashu@users.noreply.github.com>
2025-05-18 21:20:24 +0100

    Add project files.

---
+++ ServerManagement/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace ServerManagement.Models
+{
+    public class Server
+    {
+        public Server()
+        {
+            Random random = new Random();
+            int randomNumnber = random.Next(0, 2);
+            IsOnline = randomNumnber != 0;
+        }
+        public int ServerId { get; set; }
+        public bool IsOnline { get; set; }
+        public string? Name { get; set; }
+        public string? City { get; set; }
+    }
+}
