23876e7f0f51f4108174be5fabc0171fd0e6adf1
ravishashu <56905495+ravishashu@users.noreply.github.com>
2025-05-20 18:10:51 +0100

    Form Validation and Navigation

--- ServerManagement/Models/Server.cs
+++ ServerManagement/Models/Server.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ServerManagement.Models
 {
     public class Server
     {
@@ -10,7 +12,9 @@
         }
         public int ServerId { get; set; }
         public bool IsOnline { get; set; }
+        [Required]
         public string? Name { get; set; }
+        [Required]
         public string? City { get; set; }
     }
 }
