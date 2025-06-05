365654a1ee6686101dd1adfc792f78a90b426eef
ravishashu <56905495+ravishashu@users.noreply.github.com>
2025-06-05 17:32:57 +0100

    Create ToDo Project and add ToDo list page

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        //Add commentMore actions
+        public string Name { get; set; } = "";
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
+
+
